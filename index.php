f6333583a6add4e8f4c9020abd81507977894f1e
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-28 12:49:50 -0500

    working

--- index.php
+++ index.php
@@ -14,6 +14,6 @@ print_r($statuses[0]->text);
 print_r('<br><br><br>');
 print_r($statuses[0]->created_at);
 print_r('<br><br><br>');
-print_r($statuses[0]->user->id);
+print_r($statuses[0]->user->screen_name);
 print_r('<br><br><br>');
 print_r($statuses);
