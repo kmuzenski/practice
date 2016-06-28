fca85f20deebd3434a4887bfabe275e8f6068a7c
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-28 13:28:11 -0500

    testing search query

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $access_token = "738509656247668736-abFyHNW7zCgrGquLhdavrlBdMYaWGtY";
 $access_token_secret = "HUsR4g0m6hqQwSVhv5QNsnV2Y0DufZQhinLqXdUn9OUlR";
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
-$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+//$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+$statuses = $connection->get("search/tweets", ["q" => "twitterapi"]);
 
 print_r($statuses[0]->text);
 print_r('<br><br><br>');
