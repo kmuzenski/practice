8fd0a29069d7ef5e6a6b1a8a54692af6f4a22361
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-28 11:49:18 -0500

    testing twitter oauth

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+define("CONSUMER_KEY",    "Pm0OoEbrP00cjJsgGhFtnv8ES");
+define("CONSUMER_SECRET", "sD0ffwvWmEeFU6KHvXVlxOazIgICgWwxzWb4hKxPA1WTqL3I7e");
+$access_token = "738509656247668736-abFyHNW7zCgrGquLhdavrlBdMYaWGtY";
+$access_token_secret = "HUsR4g0m6hqQwSVhv5QNsnV2Y0DufZQhinLqXdUn9OUlR";
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
+$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+print_r($statuses);
